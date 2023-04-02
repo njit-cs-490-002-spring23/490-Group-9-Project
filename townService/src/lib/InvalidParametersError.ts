1dbd004595f68326e6bfd98da4ef5087bcdf7091
Paarth-Patel9 <127326715+Paarth-Patel9@users.noreply.github.com>
2023-04-02 09:01:16 -0400

    Delete townService directory

--- townService/src/lib/InvalidParametersError.ts
+++
@@ -1,8 +0,0 @@
-export default class InvalidParametersError extends Error {
-  public message: string;
-
-  public constructor(message: string) {
-    super(message);
-    this.message = message;
-  }
-}
